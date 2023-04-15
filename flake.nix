f66c6f0c0eb7ca3e0a7212b58bbfbd72968be7f0
emattiza <emattiza@gmail.com>
2023-04-15 15:20:17 -0500

    added a source dir

--- flake.nix
+++ flake.nix
@@ -8,21 +8,32 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.myapp;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+      inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages = {
+        myapp = mkPoetryApplication {projectDir = self;};
+        default = self.packages.${system}.myapp;
+      };
 
-        devShells.default = pkgs.mkShell {
-          packages = [ poetry2nix.packages.${system}.poetry ];
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        packages = [
+          poetry2nix.packages.${system}.poetry
+          (pkgs.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            editablePackageSources = {
+              myapp = ./twisted-gh-keys;
+            };
+          })
+        ];
+      };
+    });
 }
